8673acb8d25689def0e3128396b8e2f88b3705b7
John McMaster <JohnDMcMaster@gmail.com>
2013-11-08 22:10:15 -0800

    hack in code to do buffered printf

--- usart.c
+++ usart.c
@@ -35,21 +35,25 @@ an ESC character, an two byte sequence of ESC and octal 335 (decimal
 221) is sent instead.  When the last byte in the packet has been
 sent, an END character is then transmitted."
 */
-#define SLIP_END        192
-#define SLIP_ESC        219
 
 #include <libopencm3/stm32/f1/rcc.h>
 #include <libopencm3/stm32/f1/gpio.h>
 #include <libopencm3/stm32/usart.h>
+#include <libopencm3/cm3/nvic.h>
 #include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
 
 #if 1
-#define dbg(x, ...)     printf("DBG %s:%d: " x "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define dbg(x, ...)     printf("DBG %s:%d: " x "\r\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #else
 #define dbg(...)
 #endif
+
+#define SLIP_END        192
+#define SLIP_ESC        219
+
+#define BUFFER_SIZE 1024
 
 typedef struct {
     /*
@@ -101,6 +105,37 @@ axis_t g_axes[3];
 #define N_AXES      3
 bool g_escape;
 
+struct ring output_ring;
+uint8_t output_ring_buffer[BUFFER_SIZE];
+
+#define USART_CONSOLE       USART1
+
+/******************************************************************************
+ * Simple ringbuffer implementation from open-bldc's libgovernor that
+ * you can find at:
+ * https://github.com/open-bldc/open-bldc/tree/master/source/libgovernor
+ *****************************************************************************/
+
+typedef int32_t ring_size_t;
+
+struct ring {
+	uint8_t *data;
+	ring_size_t size;
+	uint32_t begin;
+	uint32_t end;
+};
+
+#define RING_SIZE(RING)  ((RING)->size - 1)
+#define RING_DATA(RING)  (RING)->data
+#define RING_EMPTY(RING) ((RING)->begin == (RING)->end)
+
+
+static void ring_init(struct ring *ring, uint8_t *buf, ring_size_t size);
+//static int32_t ring_write_ch(struct ring *ring, uint8_t ch);
+//static int32_t ring_write(struct ring *ring, uint8_t *data, ring_size_t size);
+//static int32_t ring_read_ch(struct ring *ring, uint8_t *ch);
+//static int32_t ring_read(struct ring *ring, uint8_t *data, ring_size_t size);
+
 uint8_t checksum(const uint8_t *data, size_t data_size) {
     uint8_t ret = 0;
     for (unsigned int i = 0; i < data_size; ++i) {
@@ -121,11 +156,53 @@ static void clock_setup(void)
     rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_USART1EN);
 }
 
+#if 0
 static void usart_setup(void)
 {
+	/* Initialize output ring buffer. */
+	ring_init(&output_ring, output_ring_buffer, BUFFER_SIZE);
+
+	/* Enable the USART1 interrupt. */
+	nvic_enable_irq(NVIC_USART1_IRQ);
+
+	/* Setup GPIO pin GPIO_USART1_RE_TX on GPIO port B for transmit. */
+	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
+		      GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX);
+
+	/* Setup GPIO pin GPIO_USART1_RE_RX on GPIO port B for receive. */
+	gpio_set_mode(GPIOA, GPIO_MODE_INPUT,
+		      GPIO_CNF_INPUT_FLOAT, GPIO_USART1_RX);
+
+	/* Setup UART parameters. */
+	usart_set_baudrate(USART1, 230400);
+	usart_set_databits(USART1, 8);
+	usart_set_stopbits(USART1, USART_STOPBITS_1);
+	usart_set_parity(USART1, USART_PARITY_NONE);
+	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+	usart_set_mode(USART1, USART_MODE_TX_RX);
+
+	/* Enable USART1 Receive interrupt. */
+	USART_CR1(USART1) |= USART_CR1_RXNEIE;
+
+	/* Finally enable the USART. */
+	usart_enable(USART1);
+}
+#endif
+
+static void usart_setup(void)
+{
+	/* Initialize output ring buffer. */
+	ring_init(&output_ring, output_ring_buffer, BUFFER_SIZE);
+	
+	/* Enable the USART1 interrupt. */
+	nvic_enable_irq(NVIC_USART1_IRQ);
+	
     /* Setup GPIO pin GPIO_USART1_TX/GPIO9 on GPIO port A for transmit. */
     gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
               GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX);
+	/* Setup GPIO pin GPIO_USART1_RE_RX on GPIO port A for receive. */
+	gpio_set_mode(GPIOA, GPIO_MODE_INPUT,
+		      GPIO_CNF_INPUT_FLOAT, GPIO_USART1_RX);
 
     /* Setup UART parameters. */
     // usart_set_baudrate(USART1, 38400);
@@ -141,16 +218,6 @@ static void usart_setup(void)
 
     /* Finally enable the USART. */
     usart_enable(USART1);
-}
-
-static void gpio_setup(void)
-{
-    //blue lower right
-    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
-              GPIO_CNF_OUTPUT_PUSHPULL, GPIO8);
-    //green
-    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
-              GPIO_CNF_OUTPUT_PUSHPULL, GPIO9);
 }
 
 /*
@@ -172,25 +239,116 @@ void usart_enable_rx_interrupt(uint32_t usart);
 void usart_disable_rx_interrupt(uint32_t usart);
 */
 
-#define USART_CONSOLE       USART1
 
+void ring_init(struct ring *ring, uint8_t *buf, ring_size_t size)
+{
+	ring->data = buf;
+	ring->size = size;
+	ring->begin = 0;
+	ring->end = 0;
+}
 
-//for printf
+static int32_t ring_write_ch(struct ring *ring, uint8_t ch)
+{
+	if (((ring->end + 1) % ring->size) != ring->begin) {
+		ring->data[ring->end++] = ch;
+		ring->end %= ring->size;
+		return (uint32_t)ch;
+	}
+
+	return -1;
+}
+
+static int32_t ring_write(struct ring *ring, uint8_t *data, ring_size_t size)
+{
+	int32_t i;
+
+	for (i = 0; i < size; i++) {
+		if (ring_write_ch(ring, data[i]) < 0)
+			return -i;
+	}
+
+	return i;
+}
+
+static int32_t ring_read_ch(struct ring *ring, uint8_t *ch)
+{
+	int32_t ret = -1;
+
+	if (ring->begin != ring->end) {
+		ret = ring->data[ring->begin++];
+		ring->begin %= ring->size;
+		if (ch)
+			*ch = ret;
+	}
+
+	return ret;
+}
+
+/* Not used!
+static int32_t ring_read(struct ring *ring, uint8_t *data, ring_size_t size)
+{
+	int32_t i;
+
+	for (i = 0; i < size; i++) {
+		if (ring_read_ch(ring, data + i) < 0)
+			return i;
+	}
+
+	return -i;
+}
+*/
+void usart1_isr(void)
+{
+	/* Check if we were called because of RXNE. */
+	if (((USART_CR1(USART1) & USART_CR1_RXNEIE) != 0) &&
+	    ((USART_SR(USART1) & USART_SR_RXNE) != 0)) {
+
+		/* Indicate that we got data. */
+		//gpio_toggle(GPIOC, GPIO12);
+
+		/* Retrieve the data from the peripheral. */
+		ring_write_ch(&output_ring, usart_recv(USART1));
+
+		/* Enable transmit interrupt so it sends back the data. */
+		USART_CR1(USART1) |= USART_CR1_TXEIE;
+	}
+
+	/* Check if we were called because of TXE. */
+	if (((USART_CR1(USART1) & USART_CR1_TXEIE) != 0) &&
+	    ((USART_SR(USART1) & USART_SR_TXE) != 0)) {
+
+		int32_t data;
+
+		data = ring_read_ch(&output_ring, NULL);
+
+		if (data == -1) {
+			/* Disable the TXE interrupt, it's no longer needed. */
+			USART_CR1(USART1) &= ~USART_CR1_TXEIE;
+		} else {
+			/* Put data into the transmit register. */
+			usart_send(USART1, data);
+		}
+	}
+}
+
 int _write(int file, char *ptr, int len)
 {
-    int i;
+	int ret;
 
-    if (file == STDOUT_FILENO || file == STDERR_FILENO) {
-        for (i = 0; i < len; i++) {
-            if (ptr[i] == '\n') {
-                usart_send_blocking(USART_CONSOLE, '\r');
-            }
-            usart_send_blocking(USART_CONSOLE, ptr[i]);
-        }
-        return i;
-    }
-    errno = EIO;
-    return -1;
+	if (file == 1) {
+		ret = ring_write(&output_ring, (uint8_t *)ptr, len);
+
+		if (ret < 0)
+			ret = -ret;
+
+		USART_CR1(USART1) |= USART_CR1_TXEIE;
+
+		return ret;
+	}
+
+	errno = EIO;
+	return -1;
 }
 
 void service_axis(axis_t *axis) {
@@ -402,6 +560,17 @@ const axis_t axes_init[3] = {
         .dir_gpios = GPIO5,
     },
 };
+
+
+static void gpio_setup(void)
+{
+    //blue lower right
+    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
+              GPIO_CNF_OUTPUT_PUSHPULL, GPIO8);
+    //green
+    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
+              GPIO_CNF_OUTPUT_PUSHPULL, GPIO9);
+}
 
 int main(void) {
     clock_setup();
